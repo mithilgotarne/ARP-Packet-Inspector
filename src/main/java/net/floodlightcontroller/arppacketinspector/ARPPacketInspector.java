29115cc65507a4cbe66c7f2e0723ae5545d5342e
mithil <mithilgotarne@gmail.com>
2018-11-09 00:58:26 -0600

    Added ARPPacketInspector Module

---
+++ src/main/java/net/floodlightcontroller/arppacketinspector/ARPPacketInspector.java
@@ -0,0 +1,130 @@
+package net.floodlightcontroller.arppacketinspector;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFType;
+import org.projectfloodlight.openflow.types.MacAddress;
+import org.projectfloodlight.openflow.types.VlanVid;
+import org.projectfloodlight.openflow.types.EthType;
+
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.ARP;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ARPPacketInspector implements IOFMessageListener, IFloodlightModule {
+
+    protected IFloodlightProviderService floodlightProvider;
+    protected static Logger logger;
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return ARPPacketInspector.class.getSimpleName();
+    }
+
+    @Override
+    public boolean isCallbackOrderingPrereq(OFType type, String name) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isCallbackOrderingPostreq(OFType type, String name) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void init(FloodlightModuleContext context) throws FloodlightModuleException {
+        floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+        logger = LoggerFactory.getLogger(ARPPacketInspector.class);
+    }
+
+    @Override
+    public void startUp(FloodlightModuleContext context) {
+        floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+    }
+
+    /*
+     * Overridden IOFMessageListener's receive() function.
+     */
+    @Override
+    public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+        switch (msg.getType()) {
+            case PACKET_IN:
+                /* Retrieve the deserialized packet in message */
+                Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+
+                /* Various getters and setters are exposed in Ethernet */
+//                MacAddress srcMac = eth.getSourceMACAddress();
+//                VlanVid vlanId = VlanVid.ofVlan(eth.getVlanID());
+
+                /*
+                 * Check the ethertype of the Ethernet frame and retrieve the appropriate payload.
+                 * Note the shallow equality check. EthType caches and reuses instances for valid types.
+//                 */
+//                if (eth.getEtherType() == EthType.IPv4) {
+//                    /* We got an IPv4 packet; get the payload from Ethernet */
+//                    IPv4 ipv4 = (IPv4) eth.getPayload();
+//
+//                    /* Various getters and setters are exposed in IPv4 */
+//                    byte[] ipOptions = ipv4.getOptions();
+//                    IPv4Address dstIp = ipv4.getDestinationAddress();
+//
+//                    /* Still more to come... */
+//
+//                } else
+
+                logger.info("MAC Address: {} seen on switch: {}", eth.getSourceMACAddress().toString(), sw.getId().toString());
+
+                if (eth.getEtherType() == EthType.ARP) {
+                    /* We got an ARP packet; get the payload from Ethernet */
+                    ARP arp = (ARP) eth.getPayload();
+
+                    /* Various getters and setters are exposed in ARP */
+                    boolean gratuitous = arp.isGratuitous();
+
+
+                } else {
+                    /* Unhandled ethertype */
+                }
+                break;
+            default:
+                break;
+        }
+        return Command.CONTINUE;
+    }
+
+}
