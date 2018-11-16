69b62e52ee57593278db2931d08f4119c949c82e
Matthew Valencia <MatthewLuisValencia@gmail.com>
2018-11-15 19:22:52 -0600

    Updated PacketInspector, still need controller's pair checks.

--- src/main/java/net/floodlightcontroller/arppacketinspector/ARPPacketInspector.java
+++ src/main/java/net/floodlightcontroller/arppacketinspector/ARPPacketInspector.java
@@ -2,14 +2,15 @@ package net.floodlightcontroller.arppacketinspector;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.Map;
 
+import net.floodlightcontroller.devicemanager.IDeviceService;
+import net.floodlightcontroller.devicemanager.internal.Device;
+import net.floodlightcontroller.devicemanager.internal.DeviceManagerImpl;
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFType;
-import org.projectfloodlight.openflow.types.ArpOpcode;
-import org.projectfloodlight.openflow.types.MacAddress;
-import org.projectfloodlight.openflow.types.VlanVid;
-import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.*;
 
 
 import net.floodlightcontroller.core.FloodlightContext;
@@ -126,12 +127,13 @@ public class ARPPacketInspector implements IOFMessageListener, IFloodlightModule
 
                     logger.info("ARP Sender Protocol Address: {} seen on switch: {}", arp.getSenderProtocolAddress().toString(), sw.getId().toString());
 
-                    /* search controller, retrieve IPmatch for ARPsourceHardwareAddress
-                    if(IPmatch  != arp.getSenderProtocolAddress()) { //Rule 2
+                    //*search controller for device matching given MAC and IP pair. If it exists, returns it in the iterator. If not a valid pair, iterator will be empty.
+                    DeviceManagerImpl man = new DeviceManagerImpl();
+                    Iterator senderIterator = man.queryDevices(arp.getSenderHardwareAddress(), null, arp.getSenderProtocolAddress(), IPv6Address.NONE, DatapathId.NONE, OFPort.ZERO);
+                    if(!senderIterator.hasNext()) {
                         logger.info("Rule 2 Triggered");
                         //spoofDetected
                     }
-                     */
 
                     if(arp.getOpCode() == ArpOpcode.REQUEST) {
                        if(!eth.isBroadcast()) { //Rule 3a, request should be a broadcast
@@ -153,12 +155,13 @@ public class ARPPacketInspector implements IOFMessageListener, IFloodlightModule
 
                         logger.info("ARP Target Protocol Address: {} seen on switch: {}", arp.getTargetProtocolAddress().toString(), sw.getId().toString());
 
-                        /* search controller, retrieve IPmatch for ARPtargetHardwareAddress
-                        if(IPmatch  != arp.getTargetProtocolAddress()) { //Rule 5
+                        //*search controller for device matching given MAC and IP pair. If it exists, returns it in the iterator. If not a valid pair, iterator will be empty.
+                        Iterator targetIterator = man.queryDevices(arp.getTargetHardwareAddress(), null, arp.getTargetProtocolAddress(), IPv6Address.NONE, DatapathId.NONE, OFPort.ZERO);
+                        if(!targetIterator.hasNext()) {
                             logger.info("Rule 5 Triggered");
                             //spoofDetected
                         }
-                        */
+
                     }
                 } else {
                     /* Unhandled ethertype */
